44087b61d411828d1514f7b0dee18553804bea90
KarlaT <toska6001@gmail.com>
2024-09-14 01:41:12 -0600

    Cambio del Front

--- spectra/urls.py
+++ spectra/urls.py
@@ -14,6 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
@@ -23,3 +26,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', chat)
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
