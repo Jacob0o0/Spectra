9436628c8d1ec21b9432d0616e7c4f2c1bf9b0a5
Jacob0o0 <jacoboe127@gmail.com>
2024-09-15 04:53:04 -0600

    La app está terminada

--- spectra/spotify/api.py
+++ spectra/spotify/api.py
@@ -1,13 +1,35 @@
+import re
 import requests
 import base64
 import spacy
+import random
 
 # Cargar el modelo en inglés
 nlp = spacy.load('en_core_web_sm')
 
+# Carga del modelo de PLN
+nlp = spacy.load('en_core_web_sm')
+
+# Lista opcional de nombres de bandas comunes
+known_bands = ["ACDC", "Led Zeppelin", "The Beatles", "Nirvana", "Pink Floyd", "Radiohead", "Daft punk"]
+
 def extract_artist_names(text):
     doc = nlp(text)
-    artist_names = [ent.text for ent in doc.ents if ent.label_ == 'PERSON']
+    
+    # Extraer nombres de personas o organizaciones usando PLN
+    artist_names = [ent.text for ent in doc.ents if ent.label_ in ('PERSON', 'ORG')]
+    
+    # Expresión regular para buscar posibles nombres de bandas en mayúsculas
+    potential_bands = re.findall(r'\b[A-Z][A-Z]+\b(?:\s[A-Z][A-Z]+)*', text)
+
+    # Añadir los nombres de bandas conocidos y cualquier banda detectada por la expresión regular
+    artist_names += [band for band in potential_bands if band not in artist_names]
+
+    # Comprobar también si alguna banda conocida está presente en el texto
+    for band in known_bands:
+        if band in text and band not in artist_names:
+            artist_names.append(band)
+
     return artist_names
 
 def get_access_token():
@@ -34,12 +56,45 @@ def get_top_tracks_by_artist(artist):
 
     name_artista = extract_artist_names(artist)
 
-    params = {
-        "q": name_artista,
-        "type": "artist",
-        "limit": 1
-    }
+    # Verifica si la lista está vacía
+    if not name_artista:
+        print("La lista está vacía.")
+    else:
+        select_artist = random.choice(name_artista)
 
-    response = requests.get(url, headers=headers, params=params)
-    response_data = response.json()
-    return response_data
+        params = {
+            "q": select_artist,
+            "type": "artist",
+            "limit": 1
+        }
+
+        response = requests.get(url, headers=headers, params=params)
+        response_data = response.json()
+
+        artist = response_data['artists']['items'][0]
+
+        id_artist = artist['id']
+        name_artist = artist['name']
+        img_artist = artist['images'][0]['url']
+
+        url = f'https://api.spotify.com/v1/artists/{id_artist}/top-tracks'
+        top_tracks = requests.get(url, headers=headers)
+        top_tracks_data = top_tracks.json()
+
+        tracks = []
+        for track in top_tracks_data['tracks']:
+            track_json = {}
+            track_json['album_name'] = track['album']['name']
+            track_json["album_img"] = track['album']['images'][0]["url"]
+            track_json["uri"] = track['uri']
+            track_json['track_name'] = track['name']
+            tracks.append(track_json)
+
+        # Aquí puedes procesar el mensaje como lo necesites
+        recomendation_data = {
+            'name_artist': name_artist,  # Agregada la coma aquí
+            'img_artist': img_artist,
+            'tracks': tracks
+        }
+
+        return recomendation_data
